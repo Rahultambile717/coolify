4d93fd2f43e633888c4560fe72b817f16a2bce60
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-03-16 23:08:49 +0100

    feat(core): create empty skeleton

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,1 @@
-import { initializeTerminalComponent } from './terminal.js';
-
-['livewire:navigated', 'alpine:init'].forEach((event) => {
-    document.addEventListener(event, () => {
-        // tree-shaking
-        if (document.getElementById('terminal-container')) {
-            initializeTerminalComponent()
-        }
-    });
-});
+import './bootstrap';
